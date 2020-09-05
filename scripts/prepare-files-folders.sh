81b908ee10feb169b55a863ccc4654f0e031b1ce
Meir Gabay <meir@prodops.io>
2020-09-05 19:54:56 +0300

    update pipelines

--- scripts/prepare-files-folders.sh
+++ scripts/prepare-files-folders.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 _LIVE_DIR=${LIVE_DIR:=live}
-_BACKEND_TPL_PATH=${BACKEND_TPL_PATH:=backend.tf.tpl}
+_BACKEND_TPL=${BACKEND_TPL:=backend.tf.tpl}
 
 if [[ ! -d "$_LIVE_DIR" ]]; then
-    if [[ ! -f "${_LIVE_DIR}/${_BACKEND_TPL_PATH}" ]]; then
-        echo "[ERROR] The file backend.tf.tpl doesn't exist - $_BACKEND_TPL_PATH"
+    if [[ ! -f "${_LIVE_DIR}/${_BACKEND_TPL}" ]]; then
+        echo "[ERROR] The file backend.tf.tpl doesn't exist - $_BACKEND_TPL"
         exit
     fi
 else
@@ -27,10 +27,10 @@ BRANCH_NAME=${BRANCH_NAME//\//-}
 [[ -d "$BRANCH_NAME" ]] && rm -r "$BRANCH_NAME"
 mkdir -p "${BRANCH_NAME}"/
 cp "${_LIVE_DIR}"/* "${BRANCH_NAME}"/
-sed -i.bak 's~AWS_REGION~'"$AWS_REGION"'~' "${BRANCH_NAME}/${_BACKEND_TPL_PATH}"
-sed -i.bak 's~APP_NAME~'"$TF_VAR_app_name"'~' "${BRANCH_NAME}/${_BACKEND_TPL_PATH}"
-sed -i.bak 's~ENVIRONMENT~'"$BRANCH_NAME"'~' "${BRANCH_NAME}/${_BACKEND_TPL_PATH}"
-mv "${BRANCH_NAME}/${_BACKEND_TPL_PATH}" "${BRANCH_NAME}"/backend.tf
+sed -i.bak 's~AWS_REGION~'"$AWS_REGION"'~' "${BRANCH_NAME}/${_BACKEND_TPL}"
+sed -i.bak 's~APP_NAME~'"$TF_VAR_app_name"'~' "${BRANCH_NAME}/${_BACKEND_TPL}"
+sed -i.bak 's~ENVIRONMENT~'"$BRANCH_NAME"'~' "${BRANCH_NAME}/${_BACKEND_TPL}"
+mv "${BRANCH_NAME}/${_BACKEND_TPL}" "${BRANCH_NAME}"/backend.tf
 echo "[LOG] Prepared files and folders for the environment - $BRANCH_NAME"
 ls -lah "$BRANCH_NAME"
 cat "${BRANCH_NAME}"/backend.tf
